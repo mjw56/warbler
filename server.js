01e7f61587398d7a4629a87499be5fb25f5780a8
Mike Wilcox <mwilcox56@gmail.com>
2016-01-27 20:59:41 -0500

    base setup

---
+++ server.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var config = require('./webpack.config');
+
+var app = new (require('express'))();
+var socketio = require('socket.io');
+var http = require('http');
+var port = 3000
+
+var compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+var server = http.createServer(app).listen(port, function(error) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+  }
+});
+
+var io = socketio.listen(server);
+
+io.on('connection', function(socket) {
+  console.log('socket connection');
+});
