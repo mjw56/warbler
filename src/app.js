170a9dcf1eff6652ac635d2175a7f913aa316073
Mike Wilcox <mwilcox56@gmail.com>
2016-01-30 04:54:28 -0500

    hook-in to redux

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import { Provider } from 'react-redux';
+import { App } from './containers/App';
+import configureStore from './store/configureStore';
 
-class App extends Component {
-  render() {
-    return (
-      <div>Hello World</div>
-    );
-  }
-}
+const store = configureStore();
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
