1f7a2315fdcfe91f60433908c11bb9cca536d895
Mike Wilcox <mwilcox56@gmail.com>
2016-02-02 22:34:44 -0500

    hook-in DevTools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import rootReducer from '../reducers';
+import DevTools from '../containers/DevTools';
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState)
+  const store = createStore(
+    rootReducer, 
+    initialState,
+    compose(
+      DevTools.instrument()
+    )
+  );
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
