0ae90338b26b3a1342f42a210cb2227c83595684
Mike Wilcox <mwilcox56@gmail.com>
2016-02-04 20:49:49 -0500

    update to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,6 @@ module.exports = {
     publicPath: '/static/'
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
