13184bfb563a5ec8a4acfda71a7fc8858e991b04
Mike Wilcox <mwilcox56@gmail.com>
2016-02-07 20:59:44 -0500

    wrap data into immutable struct

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,16 +1,17 @@
-import { ADD_TWEET } from '../constants/ActionTypes'
+import { ADD_TWEET } from '../constants/ActionTypes';
+import { fromJS } from 'immutable';
 
 const initialState = []
 
 export default function todos(state = initialState, action) {
   switch (action.type) {
     case ADD_TWEET:
-      return [
+      return fromJS([
         action.tweet, 
         ...state
-      ]
+      ])
 
     default:
-      return state
+      return fromJS(state)
   }
 }
