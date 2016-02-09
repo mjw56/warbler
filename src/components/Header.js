09c465523b27d14559349c637fdfe64acfd63100
Mike Wilcox <mwilcox56@gmail.com>
2016-02-08 21:08:46 -0500

    add dummy header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ImmutableComponent } from './ImmutableComponent';
+
+export class Header extends ImmutableComponent {
+  render() {
+    return (
+      <div>Hello World!</div>
+    )
+  }
+}
