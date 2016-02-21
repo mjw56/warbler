e84b6978aab3b87b896c94d9cb52d1b91bb3c3fe
Mike Wilcox <mwilcox56@gmail.com>
2016-02-21 13:47:07 -0500

    add Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { ImmutableComponent } from './ImmutableComponent';
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavDropdown,
+  MenuItem
+} from 'react-bootstrap';
 
 export class Header extends ImmutableComponent {
   render() {
     return (
-      <div>Hello World!</div>
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a>Twitter</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1} href="#">Notifications</NavItem>
+          <NavItem eventKey={1} href="#">Messages</NavItem>
+        </Nav>
+      </Navbar>
     )
   }
 }
