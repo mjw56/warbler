655e8dffce392f397e56ea46c125f5ea2aaa1c08
Mike Wilcox <mwilcox56@gmail.com>
2016-02-21 16:21:23 -0500

    add input and button, send data to server via socket

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { ImmutableComponent } from './ImmutableComponent';
 import {
+  Button,
+  Input,
   Navbar,
   Nav,
   NavItem,
   NavDropdown,
   MenuItem
 } from 'react-bootstrap';
+import io from 'socket.io-client';
 
 export class Header extends ImmutableComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+  }
+
+  _handleChange() {
+    this.setState({
+      input: this.refs.input.getValue()
+    });
+  }
+
+  _handleSearch(e) {
+    this.props.socket.emit(
+      'twitter:update:search', 
+      this.refs.input.getValue()
+    );
+  }
+
   render() {
     return (
       <Navbar>
@@ -21,7 +44,22 @@ export class Header extends ImmutableComponent {
           <NavItem eventKey={1} href="#">Notifications</NavItem>
           <NavItem eventKey={1} href="#">Messages</NavItem>
         </Nav>
+        <Navbar.Collapse>
+          <Navbar.Form pullLeft>
+            <Input type="text" 
+              ref="input"
+              onChange={this._handleChange.bind(this)}
+              placeholder="Search"
+            />
+            {' '}
+            <Button type="submit" onClick={this._handleSearch.bind(this)}>Search</Button>
+          </Navbar.Form>
+        </Navbar.Collapse>
       </Navbar>
     )
   }
 }
+
+Header.propTypes = {
+  socket: PropTypes.object
+};
