5b7e7ef24b1de2a70be48bc108ff003dd1c73446
Mike Wilcox <mwilcox56@gmail.com>
2016-02-23 06:17:26 -0500

    add the Profile Image component

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,9 +1,18 @@
 import React, { PropTypes } from 'react';
+import { ProfileImage } from './index';
 
-export const Tweet = (props) => (
-  <li>{props.tweet}</li>
-);
+export const Tweet = (props) => {
+  const image = props.tweet.get('user');
+
+  return (
+    <li>
+      <ProfileImage 
+        image={image.get('profile_image_url')} />
+      <p>{props.tweet.get('text')}</p>
+    </li>
+  );
+};
 
 Tweet.propTypes = {
-  tweet: PropTypes.string
+  tweet: PropTypes.object
 };
