beb2ddd0f5d4fc7026adf439ddbf140a916caa81
Mike Wilcox <mwilcox56@gmail.com>
2016-02-25 22:32:09 -0500

    add immutable console log

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import { Provider } from 'react-redux';
+import { IndexRoute, Router, Route, browserHistory } from 'react-router';
 import App from './containers/App';
 import configureStore from './store/configureStore';
+import 'immutable-console-log';
 var css = require("../node_modules/bootstrap/dist/css/bootstrap.css");
 
 const store = configureStore();
