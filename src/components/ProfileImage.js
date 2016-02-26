7c89e5f816a1c4c648565913e64daa833bc0a9fd
Mike Wilcox <mwilcox56@gmail.com>
2016-02-25 20:39:12 -0500

    remove console log

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -2,7 +2,6 @@ import React, { PropTypes } from 'react';
 import { Tweet } from './Tweet';
 
 export const ProfileImage = (props) => {
-  console.log('got this:', props)
   return (
     <img src={props.image} style={{ borderRadius: 5 }}/>
   );
